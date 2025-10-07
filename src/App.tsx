d18f36896d5f11466418ef4e07b16fd9e159e481
Yomna Elshorbagy <yumnamohamed30@gmail.com>
2025-10-07 05:31:44 +0300

    applay routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,83 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import ProtectedRoutes from "./Shared/ProtectedRoutes/ProtectedRoutes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Toaster } from "react-hot-toast";
+import Layout from "./Components/Layout/layout";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import Cart from "./Pages/Cart/Cart";
+import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+import About from "./Pages/About/About";
+import UserProfile from "./Pages/Profile/UserProfile";
+import NotFound from "./Components/NotFound/NotFound";
+import Category from "./Pages/Category/Category";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "productDetails/:id",
+        element: <ProductDetails />,
+      },
+      {
+        path: "category",
+        element: <Category />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+
+      {
+        path: "profile",
+        element: <UserProfile />,
+      },
+      { path: "qr-login", element: <UserProfile /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  }, //end Routes
+]);
 
 function App() {
+  const queryClient = new QueryClient();
 
   return (
     <>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <Toaster />
+      </QueryClientProvider>
     </>
-    );
+  );
 }
 
 export default App;
