419a7c702b1d229e48e7504503180e1e8d2b0c9a
marihan <marihantaha355@gmail.com>
2025-10-08 23:26:36 +0300

    Changes to be committed: 	modified:   index.html 	new file:   src/Components/PromoBanner/PromoBanner.tsx 	modified:   src/Pages/Home/Home.tsx 	new file:   src/assets/images/2ed section home page.jpg 	new file:   src/assets/images/flower.jpg

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Slider from "../../Components/HomeSlider/Slider";
 import CategorySlider from "../../Components/CategorySlider/CategorySlider";
+import PromoBanner from "../../Components/PromoBanner/PromoBanner";
 
 
 const Home: React.FC = () => {
@@ -8,6 +9,7 @@ const Home: React.FC = () => {
   return ( <>
       <Slider />
       <CategorySlider/>
+      <PromoBanner/>
     </>
   );
 };
