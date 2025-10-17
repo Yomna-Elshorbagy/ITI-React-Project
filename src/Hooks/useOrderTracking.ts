b91c930f567772f0c2f79e50a1d98324fd28658e
Yomna Elshorbagy <yumnamohamed30@gmail.com>
2025-10-17 07:42:31 +0300

    complete order tracking

---
+++ src/Hooks/useOrderTracking.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import type { Order } from "../Types/Order";
+
+export const useOrderTracking = (orderId: string) => {
+  const token = localStorage.getItem("accessToken");
+
+  return useQuery<Order>({
+    queryKey: ["orderTracking", orderId],
+    queryFn: async (): Promise<Order> => {
+      const res = await axios.get<{ data: Order }>(
+        `https://iti-react-backend.vercel.app/order/${orderId}`,
+        {
+          headers: { authentication: `bearer ${token}` },
+        }
+      );
+      return res.data.data;
+    },
+    enabled: !!orderId,
+  });
+};
